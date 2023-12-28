ebda2c85530968c4454d6c696970c46d4ef2476c
PouLou111 <145618235+PouLou111@users.noreply.github.com>
2023-12-28 05:24:13 +0300

    Update hexagon.cpp

--- lab_03/src/hexagon.cpp
+++ lab_03/src/hexagon.cpp
@@ -7,42 +7,42 @@ Hexagon::Hexagon(float p1_x, float p1_y, float p2_x, float p2_y, float p3_x, flo
 float p5_y, float p6_x, float p6_y){
     p1 = Point(p1_x, p1_y);
     p2 = Point(p2_x, p2_y);
-    p3 = Point(p3_x, p3_y);
+    p3 = Point(p3_x, p3_y); //определение конструктора в который передаются 6 параметров(вершин)
     p4 = Point(p4_x, p4_y);
     p5 = Point(p5_x, p5_y);
     p6 = Point(p6_x, p6_y);
 }
 
-Hexagon::Hexagon(istream& is)
+Hexagon::Hexagon(istream& is) //конструктор ввода 
 {
     is >> p1 >> p2 >> p3 >> p4 >> p5 >> p6;
 }
 
-void Hexagon::Print(ostream& os)
+void Hexagon::Print(ostream& os) //вывода
 {
     os << "Hexagon: " << p1 << " " << p2 << " " << p3 << " " << p4 << " " << p5 << " " << p6 << endl;
 }
 
-double Hexagon::Area() //правильного вшестиугольника
+double Hexagon::Area() //площадь
 {
     double a = p1.dist(p2);
     double s = ((3 * (sqrt(3))) / (2)) * (a * a);
     return s;
 }
 
-size_t Hexagon::VertexesNumber()
+size_t Hexagon::VertexesNumber() //колво вершин
 {
     return 6;
 }
 
-Hexagon::~Hexagon()
+Hexagon::~Hexagon() // деструктор
 {
     cout << "Done\n";
 }
-bool Hexagon::operator==(const Hexagon& other) const{
+bool Hexagon::operator==(const Hexagon& other) const{ //перегрузка оператора сравнения
     return p1 == other.p1 && p2 == other.p2 && p3 == other.p3 && p4 == other.p4 && p5 == other.p5 && p6 == other.p6;
 }
-Hexagon& Hexagon::operator=(const Hexagon& other) {
+Hexagon& Hexagon::operator=(const Hexagon& other) { //перегрузка оператора присваивания
     p1 = other.p1;
     p2 = other.p2;
     p3 = other.p3;
